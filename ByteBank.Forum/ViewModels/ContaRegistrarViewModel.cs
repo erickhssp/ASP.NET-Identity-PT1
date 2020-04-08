fb9346e7d5d1e5f8e3f9b041d40150fe0e587030
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-08 14:09:54 -0300

    Instalação de pacotes e criação da pagina de registro.

---
+++ ByteBank.Forum/ViewModels/ContaRegistrarViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class ContaRegistrarViewModel
+    {
+        [Required]
+
+        public String UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Nome Completo")]
+        public String NomeCompleto { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Senha { get; set; }
+
+    }
+}
