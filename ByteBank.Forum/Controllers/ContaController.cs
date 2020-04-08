540c9983dd472b566681faba5db72651ef0a63f2
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-08 18:16:23 -0300

    IdentityDbContext e IdentityUser:
    Conhecemos a classe IdentityUser já implementada no AspNet Identity e ela já possui muitas propriedades implementadas! Além disso, usamos o IdentityDbContext para criar as tabelas necessárias para o AspNet Identity de forma muito simples.
    UserStore e UserManager:
    
    Vimos a importância da interface IUserStore para a comunicação do UserManager com o banco de dados! Arquitetura do Identity .
    Arquitetura do Identity:
    
    Vimos 3 características fundamentais sobre a arquitetura do AspNet Identity: Baseado em interfaces, implementação genérica e assincronicidade!

--- ByteBank.Forum/Controllers/ContaController.cs
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -1,7 +1,11 @@
-﻿using ByteBank.Forum.ViewModels;
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,9 +20,30 @@ namespace ByteBank.Forum.Controllers
         }
 
         [HttpPost]
-        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
         {
-            return View();
+
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
+                var userStore = new UserStore<UsuarioAplicacao>(dbContext);
+                var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+
+
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+               await userManager.CreateAsync(novoUsuario, "123456");
+
+                // Podemos incluir o usuario
+                return RedirectToAction("Index", "Home");
+            }
+
+            //Alguma coisa de errado aconteceu
+            return View(modelo);
         }
     }
 }
